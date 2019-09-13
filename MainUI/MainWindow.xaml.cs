b9444135fb629496a94a28021ab68a2d814abded
xingzheng <xingzheng@hotmail.com>
2019-09-13 12:06:44 +0800

    add mainUI

---
+++ MainUI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.Foundation.Metadata;
+
+namespace MainUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 8))
+            {
+                Process.Start("WPFInkSample.exe");
+            }
+            else
+            {
+                MessageBox.Show("Since version 1903, we can use UWP control in WPF project.");
+            }
+        }
+    }
+}
